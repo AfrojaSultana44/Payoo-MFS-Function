afc1bb7973e9c898f1223fd6e34994662dbb29d8
AfrojaSultana44 <afrojasultanamoni311@gmail.com>
2024-09-21 11:47:40 +0600

    Implemented add money and cashout  using function

--- js/cashOut.js
+++ js/cashOut.js
@@ -1,17 +1,17 @@
 document
-  .getElementById("btn-add-money")
+  .getElementById("btn-cash-out")
   .addEventListener("click", function (event) {
     event.preventDefault();
 
-    const addMoney = getInputFieldValueById("input-add-money");
-    const pinNumber = getInputFieldValueById("input-pin-number");
+    const cashOut = getInputFieldValueById("input-cash-out");
+    const pinNumber = getInputFieldValueById("input-cash-out-pin");
 
     if (pinNumber === 1234) {
       const balance = getTextFieldValueById("account-balance");
-      const newBalance = balance + addMoney;
+      const newBalance = balance - cashOut;
 
       document.getElementById("account-balance").innerText = newBalance;
     } else {
-      alert("Failed to add money.");
+      alert("Failed to cash out.");
     }
   });
