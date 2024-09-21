f907173bb48df1de6a9236d76b66ef0b95207246
AfrojaSultana44 <afrojasultanamoni311@gmail.com>
2024-09-21 22:57:02 +0600

    show transaction history using dynamic dom and data validation

--- js/addMoney.js
+++ js/addMoney.js
@@ -6,11 +6,23 @@ document
     const addMoney = getInputFieldValueById("input-add-money");
     const pinNumber = getInputFieldValueById("input-pin-number");
 
+    if (isNaN(addMoney)) {
+      alert("Failed to add money.");
+      return;
+    }
+
     if (pinNumber === 1234) {
       const balance = getTextFieldValueById("account-balance");
       const newBalance = balance + addMoney;
 
       document.getElementById("account-balance").innerText = newBalance;
+
+      // add to transaction history
+      const p = document.createElement("P");
+      p.innerText = `Added: ${addMoney} Tk. New Balance: ${newBalance}`;
+
+      //  should be a common function
+      document.getElementById("transaction-container").appendChild(p);
     } else {
       alert("Failed to add money.");
     }
