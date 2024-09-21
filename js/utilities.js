f089b65a5b201952a77c049b9cfe4052bbf7656d
AfrojaSultana44 <afrojasultanamoni311@gmail.com>
2024-09-21 15:13:02 +0600

    Created features toggle button

--- js/utilities.js
+++ js/utilities.js
@@ -9,3 +9,16 @@ function getTextFieldValueById(id) {
   const textNumber = parseFloat(textValue);
   return textNumber;
 }
+
+
+function showSectionById(id){
+
+    // hide all the sections
+    document.getElementById('add-money-form').classList.add('hidden')
+    document.getElementById('cash-out-form').classList.add('hidden')
+    document.getElementById('transaction-section').classList.add('hidden')
+
+    // show the section with the provided id
+    document.getElementById(id).classList.remove('hidden')
+
+}
